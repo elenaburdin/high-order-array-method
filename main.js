f31c4f577dea841921f04d7a4741937da80ef7f7
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-01-22 16:20:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false
+    }
+];
+
+
+const todoCompleted = todos.filter(function (todo) {
+    return todo.isCompleted === true;
+}).map(function (todo) {
+    return todo.text;
+})
+
+console.log(todoCompleted)
